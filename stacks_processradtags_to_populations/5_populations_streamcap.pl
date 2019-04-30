6d946f0f07032b1af9905d394029203c0e7be33e
karenbobier <karenbobier@gmail.com>
2019-04-30 00:08:16 -0400

    change $i to $i\

--- stacks_processradtags_to_populations/5_populations_streamcap.pl
+++ stacks_processradtags_to_populations/5_populations_streamcap.pl
@@ -28,14 +28,14 @@ for i in $SPECIES
 do
 
 mkdir /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/multiple_snp
-populations -t 12 -P /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/ -M /scratch/keb27269/stream_cap/$i/$i_populationmap.txt -O /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/multiple_snp -r 0.6 -p 2 --fasta_loci --fasta_samples --fasta_samples_raw --vcf --fstats --genepop --vcf_haplotypes --phylip --phylip_var --structure --treemix
+populations -t 12 -P /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/ -M /scratch/keb27269/stream_cap/$i/$i\_populationmap.txt -O /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/multiple_snp -r 0.6 -p 2 --fasta_loci --fasta_samples --fasta_samples_raw --vcf --fstats --genepop --vcf_haplotypes --phylip --phylip_var --structure --treemix
 # -r is proportion f indvs that must have the locus, -p minimum number of populations a locus needs to be present in, other flags for output files. -fstats is for Fst,
 mkdir /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/single_snp
 #to restrict data analysis to only the first SNP per locus
-populations -t 12 -P /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/ -M /scratch/keb27269/stream_cap/$i/$i_populationmap.txt -O /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/single_snp -r 0.6 -p 2 --fasta_loci --fasta_samples --fasta_samples_raw --vcf --fstats --genepop --vcf_haplotypes --phylip --phylip_var --structure --treemix --write_single_snp
+populations -t 12 -P /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/ -M /scratch/keb27269/stream_cap/$i/$i\_populationmap.txt -O /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/single_snp -r 0.6 -p 2 --fasta_loci --fasta_samples --fasta_samples_raw --vcf --fstats --genepop --vcf_haplotypes --phylip --phylip_var --structure --treemix --write_single_snp
 
 mkdir /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/filter_snp
 # more conservative selection of loci, only export loci present in 80% of indvs and have a frequency greater than 5%.
-populations -t 12 -P /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/ -M /scratch/keb27269/stream_cap/$i/$i_populationmap.txt -O /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/filter_snp -r 0.8 -p 2 --min_maf 0.05 --fasta_loci --fasta_samples --fasta_samples_raw --vcf --fstats --genepop --vcf_haplotypes --phylip --phylip_var --structure --treemix
+populations -t 12 -P /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/ -M /scratch/keb27269/stream_cap/$i/$i\_populationmap.txt -O /scratch/keb27269/stream_cap/$i/denovo_output/denovo_n_4_catalog_new/filter_snp -r 0.8 -p 2 --min_maf 0.05 --fasta_loci --fasta_samples --fasta_samples_raw --vcf --fstats --genepop --vcf_haplotypes --phylip --phylip_var --structure --treemix
 
 done
