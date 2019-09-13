dbcb24e759654b999dbb7c5b71b0f337243a7fcc
karenbobier <karenbobier@gmail.com>
2019-09-13 14:08:27 -0400

    add verbose flag and pwd after changing directories

--- multi-dice-scripts/convert_VCF2SFS.sh
+++ multi-dice-scripts/convert_VCF2SFS.sh
@@ -22,21 +22,25 @@ module load matplotlib/2.1.2-fosscuda-2018b-Python-2.7.15
 
 export PATH=${HOME}/apps/fastsimcoal2/2.6.0.2:$PATH
 
-cp easySFS.py ./
+#cp easySFS.py ./
 chmod 777 easySFS.py
 
 
 #convert vcf to sfs
 #./easySFS -i input.vcf -p pops_file.txt --preview
 cd $basedir/L_zonistius/
-./easySFS.py -i populations.snps.vcf -p L_zonistius_populationmap.txt  -a --proj 8,8 -o L_zonistius_sfsfolded --prefix L_zonistius -f --ploidy 2
-./easySFS.py -i populations.snps.vcf -p L_zonistius_populationmap.txt  -a --proj 8,8 -o L_zonistius_sfsunfolded --prefix L_zonistius -f --unfolded --ploidy 2
+pwd
+./easySFS.py -i populations.snps.vcf -p L_zonistius_populationmap.txt  -a --proj 8,8 -o L_zonistius_sfsfolded --prefix L_zonistius -f --ploidy 2 -v
+./easySFS.py -i populations.snps.vcf -p L_zonistius_populationmap.txt  -a --proj 8,8 -o L_zonistius_sfsunfolded --prefix L_zonistius -f --unfolded --ploidy 2 -v
 cd $basedir/N_lutipinnis/
-./easySFS.py -i populations.snps.vcf -p N_lutipinnis_populationmap.txt -a --proj 8,8 -o N_lutipinnis_sfsfolded --prefix N_lutipinnis -f --ploidy 2
-./easySFS.py -i populations.snps.vcf -p N_lutipinnis_populationmap.txt -a --proj 8,8 -o N_lutipinnis_sfsunfolded --prefix N_lutipinnis -f --unfolded --ploidy 2
+pwd
+./easySFS.py -i populations.snps.vcf -p N_lutipinnis_populationmap.txt -a --proj 8,8 -o N_lutipinnis_sfsfolded --prefix N_lutipinnis -f --ploidy 2 -v
+./easySFS.py -i populations.snps.vcf -p N_lutipinnis_populationmap.txt -a --proj 8,8 -o N_lutipinnis_sfsunfolded --prefix N_lutipinnis -f --unfolded --ploidy 2 -v
 cd $basedir/N_leptocephalus/
-./easySFS.py -i populations.snps.vcf -p N_leptocephalus_populationmap.txt -a --proj 8,8 -o N_leptocephalus_sfsfolded --prefix N_leptocephalus -f --ploidy 2
-./easySFS.py -i populations.snps.vcf -p N_leptocephalus_populationmap.txt -a --proj 8,8 -o N_leptocephalus_sfsunfolded --prefix N_leptocephalus -f --unfolded --ploidy 2
+pwd
+./easySFS.py -i populations.snps.vcf -p N_leptocephalus_populationmap.txt -a --proj 8,8 -o N_leptocephalus_sfsfolded --prefix N_leptocephalus -f --ploidy 2 -v
+./easySFS.py -i populations.snps.vcf -p N_leptocephalus_populationmap.txt -a --proj 8,8 -o N_leptocephalus_sfsunfolded --prefix N_leptocephalus -f --unfolded --ploidy 2 -v
 cd $basedir/P_nigrofasciata
-./easySFS.py -i populations.snps.vcf -p P_nigrofasciata_populationmap.txt -a --proj 8,8 -o P_nigrofasciata_sfsfolded --prefix P_nigrofasciata -f --ploidy 2
-./easySFS.py -i populations.snps.vcf -p P_nigrofasciata_populationmap.txt -a --proj 8,8 -o P_nigrofasciata_sfsunfolded --prefix P_nigrofasciata -f --unfolded --ploidy 2
+pwd
+./easySFS.py -i populations.snps.vcf -p P_nigrofasciata_populationmap.txt -a --proj 8,8 -o P_nigrofasciata_sfsfolded --prefix P_nigrofasciata -f --ploidy 2 -v
+./easySFS.py -i populations.snps.vcf -p P_nigrofasciata_populationmap.txt -a --proj 8,8 -o P_nigrofasciata_sfsunfolded --prefix P_nigrofasciata -f --unfolded --ploidy 2 -v
